9f60aa9f2876920866e0db0c479434bfea601d97
JohnProphet <johnldudash@gmail.com>
2023-11-02 11:49:14 -0500

    class 15

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,18 @@
-//Code Your Solution Below
+<script>
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+        let testName = document.querySelector("input[name=testName]");
+        let testDate = document.querySelector("input[name=testDate]");
+        let rocketType = document.querySelector("input[name=rocketType]");
+        let numRocketBoosters = document.querySelector("input[name=numRocketBoosters]");
+        let windRating = document.querySelector("input[name=windRating]");
+        let productionServers = document.querySelector("input[name=productionServers]");
+        if (testName.value === "" || testDate.value === "" || rocketType === "" || numRocketBoosters === "" || windRating === "" || productionServers === "") {
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+        }
+        });
+    });
+</script>
