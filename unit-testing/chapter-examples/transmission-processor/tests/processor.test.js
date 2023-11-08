596b6200bac563e8860268911c4439315e227dec
JohnProphet <johnldudash@gmail.com>
2023-11-08 00:14:33 -0600

    tdd chapter examples redo

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -1,5 +1,29 @@
+const processor = require('../processor.js');
+
 describe("transmission processor", function() {
 
-   //  TODO: put tests here
- 
- });
+  test("takes a string and returns an object", function() {
+      let result = processor("9701::<489584872710>");
+      expect(typeof result).toBe("object");
+  });
+
+  test("returns -1 if '::' not found", function () {
+      let result = processor("9701<489584872710>");
+      expect(result).toBe(-1);
+  });
+
+  test("returns object id", function() {
+      let result = processor("9701::<48958487210>");
+      expect(result.id).not.toBeUndefined();
+  });
+   
+  test("converts id to a number", function() {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).toBe(9701);
+  });
+
+  test("returns rawData in object", () => {
+    let result = processor("9701::<487297403495720912>");
+    expect(result.rawData).not.toBeUndefined();
+  });
+});
